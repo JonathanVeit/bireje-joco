bab1c642b6991f6d3c9899e7d71e53ae1e05930a
Jonathan Veit <official.jonathanveit@gmail.com>
2021-04-27 16:27:46 +0200

    [*] removed unityeditor reference in player character input

--- Client/BiReJe JoCo/Assets/Scripts/CharacterController/PlayerCharacterInput.cs
+++ Client/BiReJe JoCo/Assets/Scripts/CharacterController/PlayerCharacterInput.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System;
-using UnityEditor.VersionControl;
 
 namespace BiReJeJoCo.Character
 {
